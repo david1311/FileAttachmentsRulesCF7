bc1c74a36ba3281703917549c73b64bdaf016c07
Luis david del barrio <ldelbarrio@optimizaclick.com>
2018-04-10 17:59:04 +0200

    update library to check if pass condition

--- lib/fileAttachemntsRulesCF7.php
+++ lib/fileAttachemntsRulesCF7.php
@@ -15,6 +15,7 @@ class fileAttachmentsRulesCF7
         add_action('admin_footer', [$this, 'testUpload']);
         add_action("wpcf7_before_send_mail", [$this, 'initCF7beforeSend']);
         add_action('save_post', [$this, 'saveAttachmentCF7']);
+        add_action('save_post', [$this, 'saveConditionAttachmentCF7']);
         add_action( 'admin_enqueue_scripts', [$this,'mediaScrips' ]);
     }
 
@@ -32,7 +33,7 @@ class fileAttachmentsRulesCF7
     }
 
     public function initCF7beforeSend($wcf7) {
-        return fileAttachmentsCF7beforeSend::updateValuesCF7beforeSend($wcf7);
+        fileAttachmentsCF7beforeSend::updateValuesCF7beforeSend($wcf7);
     }
 
     public function fileAttachmentsRulesPage() {
@@ -74,12 +75,12 @@ class fileAttachmentsRulesCF7
         $names = $this->filterByPrefix($keys, $prefix);
 
         foreach($names as $name):
-
+            $condition = $this->getAttachmentCondition($keys[$name])
             ?>
             <tr>
                 <th>[<?= $name ?>]</th>
                 <th><?= $this->getAttachmentURL($keys[$name]) ?></th>
-                <?= $prefix === 'multiple' ? '<th><input type="text"></th>' : '' ?>
+                <?= $prefix === 'multiple' ? "<th><input type='text' name='condition_$name' value='$condition'></th>" : '' ?>
             </tr>
             <?php
         endforeach;
@@ -92,8 +93,39 @@ class fileAttachmentsRulesCF7
         return get_post($item)->guid;
     }
 
+    private function getAttachmentCondition($id) {
+        $serialize = array_shift($id);
+        $condition = json_decode($serialize)->condition;
+
+        return $condition;
+    }
+
     private function setFormsToSaveImages($kind) {
-        return fileAttachmentsUpload::getUploadImageGalleryOption($kind);
+        fileAttachmentsUpload::getUploadImageGalleryOption($kind);
+    }
+
+    public function saveConditionAttachmentCF7() {
+        if(isset($_POST)) {
+            $filtered = array_filter(array_keys($_POST), function ($key) {
+                return strpos($key, 'condition_') === 0;
+            });
+
+        }
+
+        isset($filtered) ? $this->updateConditionsByPostID($filtered, $_POST['post_ID']) : [];
+    }
+
+    private function updateConditionsByPostID($filtered, $id) {
+        foreach($filtered as $key) {
+            $metaKey = str_replace('condition_', '', $key);
+            $meta = json_decode(get_post_meta($id, $metaKey)[0]);
+
+            $meta->condition = $_POST[$key];
+
+            if(!empty($_POST[$key])) {
+                update_post_meta($id, $metaKey, json_encode($meta));
+            }
+        }
     }
 
     public function saveAttachmentCF7() {
@@ -103,7 +135,7 @@ class fileAttachmentsRulesCF7
                 'condition' => ''
             ];
 
-           update_post_meta($_POST['post_ID'], $_POST['saveAttachmentCF7type'].'_attachment_cf7'. rand(), json_encode($arguments));
+       //    update_post_meta($_POST['post_ID'], $_POST['saveAttachmentCF7type'].'_attachment_cf7'. rand(), json_encode($arguments));
         }
     }
 }
--- lib/fileAttachmentsCF7beforeSend.php
+++ lib/fileAttachmentsCF7beforeSend.php
@@ -9,14 +9,18 @@
 namespace FileAttachments;
 
 
+use Hoa\Ruler\Ruler;
+
 class fileAttachmentsCF7beforeSend
 {
     public static function updateValuesCF7beforeSend(&$wpcf7_data) {
         $itemID = self::getValueByShortcode($wpcf7_data->mail['attachments']);
         $item = json_decode($itemID)->item;
 
-        $wpcf7_data->mail['attachments'] = get_post($item)->guid;
-        $wpcf7_data->skip_mail = true;
+        if(isset($item->condition) && self::isRuleValid($item->condition, $wpcf7_data)) {
+            $wpcf7_data->mail['attachments'] = get_post($item)->guid;
+            $wpcf7_data->skip_mail = true;
+        }
     }
 
     private static function getValueByShortcode($shortcode) {
@@ -28,5 +32,12 @@ class fileAttachmentsCF7beforeSend
         );
 
        return $metaValue[0]->meta_value;
+    }
+
+    private static function isRuleValid($rule, $cf7) {
+        $ruler = new Ruler();
+        $context['subject'] = $cf7->subject;
+
+        return $ruler->assert($rule, $context);
     }
 }
